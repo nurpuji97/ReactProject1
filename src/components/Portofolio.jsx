79042cd8307de72ffb3e1c389c171961d77e41f8
nurpuji97 <nurpujiyanto1997@gmail.com>
2023-03-23 13:57:24 +0700

    Portofolio: Title (Selesai) | Card (Selesai) | link (Selesai); responsive (Belum); dark Mode (Belum); translate(Belum)

---
+++ src/components/Portofolio.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Image1 from "../images/image_portofolio-1.jpg";
+import Image2 from "../images/image_portofolio-2.jpg";
+import Image3 from "../images/image_portofolio-3.jpg";
+
+const Portofolio = () => {
+  return (
+    <div className="portofolio">
+      <p className="portofolio-title">Portofolio</p>
+      <p className="portofolio-subtitle">Ini adalah karya terbaik aku</p>
+      <div className="portofolio-container">
+        {/* card portofolio */}
+        <div className="portofolio-container-card">
+          <img
+            src={Image1}
+            className="portofolio-container-card-image"
+            alt="gambar-portofolio"
+          />
+          <div className="portofolio-container-card-desc">
+            <p className="portofolio-container-card-desc-title">
+              Subrek Android
+            </p>
+            <p className="portofolio-container-card-desc-Subtitle">
+              Website untuk mempromosikan aplikasi android dibidang finansial
+              agar mempermudah mengatur finansial serta transaksi anda.
+            </p>
+            <Link to="" className="portofolio-container-card-desc-link">
+              Lihat Selengkapnya.....
+            </Link>
+          </div>
+        </div>
+
+        {/* card portofolio */}
+        <div className="portofolio-container-card">
+          <img
+            src={Image2}
+            className="portofolio-container-card-image"
+            alt="gambar-portofolio"
+          />
+          <div className="portofolio-container-card-desc">
+            <p className="portofolio-container-card-desc-title">Markey</p>
+            <p className="portofolio-container-card-desc-Subtitle">
+              Podia Podcast adalah aplikasi kumpulan konten audio untuk
+              menghibur anda. konten audio podcast terlengkap dari berbagai
+              negara.
+            </p>
+            <Link to="" className="portofolio-container-card-desc-link">
+              Lihat Selengkapnya.....
+            </Link>
+          </div>
+        </div>
+
+        {/* card portofolio */}
+        <div className="portofolio-container-card">
+          <img
+            src={Image3}
+            className="portofolio-container-card-image"
+            alt="gambar-portofolio"
+          />
+          <div className="portofolio-container-card-desc">
+            <p className="portofolio-container-card-desc-title">
+              Podia Podcast
+            </p>
+            <p className="portofolio-container-card-desc-Subtitle">
+              Podia Podcast adalah aplikasi kumpulan konten audio untuk
+              menghibur anda. konten audio podcast terlengkap dari berbagai
+              negara.
+            </p>
+            <Link to="" className="portofolio-container-card-desc-link">
+              Lihat Selengkapnya.....
+            </Link>
+          </div>
+        </div>
+
+        {/* Link Semua Project */}
+        <Link to="" className="portofolio-continue">
+          Lihat Project Lainnya........
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Portofolio;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Portofolio from "../components/Portofolio";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <About />
+      <Portofolio />
       <h1 className="text-primary-shades-300 dark:text-primary-light">
         hello World
       </h1>
