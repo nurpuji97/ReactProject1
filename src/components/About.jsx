b42f2d6b033d3c59257c16ed48dca76054b89060
nurpuji97 <nurpujiyanto1997@gmail.com>
2023-04-28 19:41:07 +0700

    What I do, kembali lagi ke awal gak pake json

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import Tabs from "../elements/tabs/Tabs";
 import Image from "../images/image.jpg";
-// import IconWeb from "../images/Icon_Webdesain.svg";
-// import IconFront from "../images/Icon_Frontend.svg";
-// import IconBack from "../images/Icon_Backend.svg";
+import IconWeb from "../images/Icon_Webdesain.svg";
+import IconFront from "../images/Icon_Frontend.svg";
+import IconBack from "../images/Icon_Backend.svg";
 
 import { useTranslation } from "react-i18next";
 
 const About = (props) => {
   let PendidikanProps = props.JsonData.Pendidikan;
-  let WorkProps = props.JsonData.Work;
-  const FilterData = Object.values(WorkProps).filter(
-    (value) => value.Lang === props.translate
-  );
 
   const { t } = useTranslation();
   return (
@@ -48,26 +44,50 @@ const About = (props) => {
         <div className="about-content-work">
           <p className="about-content-work-title">{t("WhatIdo")}</p>
           <div className="about-content-work-group_card">
-            {FilterData === 0
-              ? " "
-              : FilterData.map((Workdata) => (
-                  <div
-                    className="about-content-work-group_card-card"
-                    key={Workdata.id}
-                  >
-                    <img
-                      className="w-[42px] mb-[30px] text-center mx-auto"
-                      src={Workdata.Icon}
-                      alt="icon-card"
-                    />
-                    <p className="mb-2.5 text-[21px] font-medium dark:text-primary-tints-300">
-                      {Workdata.Name}{" "}
-                    </p>
-                    <p className="text-primary-shades-400 text-sm dark:text-primary-tints-500">
-                      {Workdata.Desc}
-                    </p>
-                  </div>
-                ))}
+            {/* card */}
+            <div className="about-content-work-group_card-card">
+              <img
+                className="w-[42px] mb-[30px] text-center mx-auto"
+                src={IconWeb}
+                alt="icon-card"
+              />
+              <p className="mb-2.5 text-[21px] font-medium dark:text-primary-tints-300">
+                {t("webDesain")}{" "}
+              </p>
+              <p className="text-primary-shades-400 text-sm dark:text-primary-tints-500">
+                {t("webDesainDesc")}
+              </p>
+            </div>
+
+            {/* card */}
+            <div className="about-content-work-group_card-card">
+              <img
+                className="w-[64px] mb-[30px] text-center mx-auto"
+                src={IconFront}
+                alt="icon-card"
+              />
+              <p className="mb-2.5 text-[21px] font-medium dark:text-primary-tints-300">
+                Frond End{" "}
+              </p>
+              <p className="text-primary-shades-400 text-sm dark:text-primary-tints-500">
+                {t("frontEndnDesc")}
+              </p>
+            </div>
+
+            {/* card */}
+            <div className="about-content-work-group_card-card">
+              <img
+                className="w-[60px] mb-[30px] text-center mx-auto"
+                src={IconBack}
+                alt="icon-card"
+              />
+              <p className="mb-2.5 text-[21px] font-medium dark:text-primary-tints-300">
+                Back End{" "}
+              </p>
+              <p className="text-primary-shades-400 text-sm dark:text-primary-tints-500">
+                {t("backEndDesc")}
+              </p>
+            </div>
           </div>
         </div>
       </div>
