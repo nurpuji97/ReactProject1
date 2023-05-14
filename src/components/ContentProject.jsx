d4a5cd7f1eb26ab00a5b7e3cd24ee943b24e9b30
nurpuji97 <nurpujiyanto1997@gmail.com>
2023-05-14 15:24:43 +0700

    modifikasi card di project card

--- src/components/ContentProject.jsx
+++ src/components/ContentProject.jsx
@@ -6,6 +6,49 @@ const ContentProject = () => {
   return (
     <div className="ContentProject">
       <div className="containerCard-Project">
+        {/* card */}
+        <div className="cardProject">
+          <img src={ImageCard} className="rounded mb-[35px]" alt="ImageCard" />
+          <p className="titleCard">Ini Aplikasi</p>
+          <p className="descCard">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, earum eos? Delectus possimus soluta dolorem tempore
+            aperiam, voluptates vitae minus.
+          </p>
+          <NavLink to="" className="btnCard">
+            Lihat Selanjutnya
+          </NavLink>
+        </div>
+
+        {/* card */}
+        <div className="cardProject">
+          <img src={ImageCard} className="rounded mb-[35px]" alt="ImageCard" />
+          <p className="titleCard">Ini Aplikasi</p>
+          <p className="descCard">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, earum eos? Delectus possimus soluta dolorem tempore
+            aperiam, voluptates vitae minus.
+          </p>
+          <NavLink to="" className="btnCard">
+            Lihat Selanjutnya
+          </NavLink>
+        </div>
+
+        {/* card */}
+        <div className="cardProject">
+          <img src={ImageCard} className="rounded mb-[35px]" alt="ImageCard" />
+          <p className="titleCard">Ini Aplikasi</p>
+          <p className="descCard">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, earum eos? Delectus possimus soluta dolorem tempore
+            aperiam, voluptates vitae minus.
+          </p>
+          <NavLink to="" className="btnCard">
+            Lihat Selanjutnya
+          </NavLink>
+        </div>
+
+        {/* card */}
         <div className="cardProject">
           <img src={ImageCard} className="rounded mb-[35px]" alt="ImageCard" />
           <p className="titleCard">Ini Aplikasi</p>
